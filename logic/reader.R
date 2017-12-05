74e271c8c6da4eedf0cdd86f043564c8f6e01e6a
CalvinHuynh <huynhck001@gmail.com>
2017-12-05 21:24:19 +0100

    added some functions

--- logic/reader.R
+++ logic/reader.R
@@ -6,7 +6,6 @@ library(tm)
 library(readtext)
 config <- config::get(file = "config.yml")
 
-
 # General functions -------------------------------------------------------
 
 convertToBinary <- function(number){
@@ -17,28 +16,37 @@ convertToBinary <- function(number){
   }
 }
 
-# Reading tab seperated file
+# Reading data ------------------------------------------------------------
+
+# Data set 1
 trainIMDBData <- read.table(config$firstDataFile, sep = '\t', header = TRUE)
 trainIMDBData$id <- sub("^", "1_", trainIMDBData$id)
 
-trainData2positive <- readtext(paste0(config$trainDataDirectoryPositive,"*"))
-trainData2positive$V3 <- 0
-trainData2positive <- trainData2positive[c("doc_id", "V3", "text")]
-colnames(trainData2positive) <- colnames(trainIMDBData)
+# Data set 2
+readSecondDataset <- function(){
+  trainData2positive <- readtext(paste0(config$trainDataDirectoryPositive,"*"))
+  trainData2positive$V3 <- 0
+  trainData2positive <- trainData2positive[c("doc_id", "V3", "text")]
+  colnames(trainData2positive) <- colnames(trainIMDBData)
+  
+  trainData2negative <- readtext(paste0(config$trainDataDirectoryNegative,"*"))
+  trainData2negative$V3 <- 0
+  trainData2negative <- trainData2negative[c("doc_id", "V3", "text")]
+  colnames(trainData2negative) <- colnames(trainIMDBData)
+  
+  combinedTrainData2 <- rbind(trainData2positive, trainData2negative)
+  
+  # Formatting the second data set
+  combinedTrainData2$id <- gsub(combinedTrainData2$id, pattern = ".txt", replacement = "")
+  combinedTrainData2$id <- sub("^", "2_", combinedTrainData2$id)
+  combinedTrainData2$sentiment <- sub(".*_","",combinedTrainData2$id)
+  combinedTrainData2$sentiment <- as.numeric(combinedTrainData2$sentiment)
+  combinedTrainData2$sentiment <- lapply(combinedTrainData2$sentiment, convertToBinary)
+  
+  return(combinedTrainData2)
+}
 
-trainData2negative <- readtext(paste0(config$trainDataDirectoryNegative,"*"))
-trainData2negative$V3 <- 0
-trainData2negative <- trainData2negative[c("doc_id", "V3", "text")]
-colnames(trainData2negative) <- colnames(trainIMDBData)
-
-combinedTrainData2 <- rbind(trainData2positive, trainData2negative)
-
-combinedTrainData2$id <- gsub(combinedTrainData2$id, pattern = ".txt", replacement = "")
-combinedTrainData2$id <- sub("^", "2_", combinedTrainData2$id)
-combinedTrainData2$sentiment <- sub(".*_","",combinedTrainData2$id)
-combinedTrainData2$sentiment <- as.numeric(combinedTrainData2$sentiment)
-combinedTrainData2$sentiment <- lapply(combinedTrainData2$sentiment, convertToBinary)
-
+combinedTrainData2 <- readSecondDataset()
 # Basic cleaning function
 commonCleaning <- function(textToClean){
   # All lowercase
@@ -73,19 +81,31 @@ removeCommonStopWords <- function(textToClean, customWords = NULL){
 }
 
 # Convert the review data using the utf-8 encoding
-trainIMDBData$review <- iconv(enc2utf8(as.character(trainIMDBData$review)),sub = "byte")
+convertToUtf8Enc <- function(text){
+  return(iconv(enc2utf8(as.character(text)),sub = "byte"))
+}
+
+trainIMDBData$review <- convertToUtf8Enc(trainIMDBData$review)
 trainIMDBData$review <- commonCleaning(trainIMDBData$review)
 trainIMDBData$review <- removeCommonStopWords(trainIMDBData$review)
 
-term_count <- freq_terms(trainIMDBData$review, 10)
-plot(term_count)
+combinedTrainData2$review <- convertToUtf8Enc(combinedTrainData2$review)
+combinedTrainData2$review <- commonCleaning(combinedTrainData2$review)
 
-trainIMDBDataSource <- VectorSource(trainIMDBData$review)
-trainIMDBDataCorpus <- VCorpus(trainIMDBDataSource)
+# term_count <- freq_terms(trainIMDBData$review, 10)
+# plot(term_count)
+
+convertTextToCorpus <- function(text){
+  vectorSource <- VectorSource(text)
+  corpus <- VCorpus(vectorSource)
+  
+  return(corpus)
+}
+
+trainIMDBDataCorpus <- convertTextToCorpus(trainIMDBData$review)
 
 train_dtm <- DocumentTermMatrix(trainIMDBDataCorpus)
 train_tdm <- TermDocumentMatrix(trainIMDBDataCorpus)
-
 
 ### Not enough memory
 train_tdm_m <- as.matrix(train_tdm)
@@ -99,3 +119,18 @@ term_tdm_freq[1:10]
 
 # Plot a barchart of the 10 most common words
 barplot(term_tdm_freq[1:10], col = "tan", las = 2)
+
+trainData2Corpus <- convertTextToCorpus(combinedTrainData2$review)
+
+train2_dtm <- DocumentTermMatrix(trainData2Corpus)
+train2_tdm <- TermDocumentMatrix(trainData2Corpus)
+
+train2_tdm_m <- as.matrix(train_tdm2)
+
+term2_tdm_freq <- rowSums(train2_tdm_m)
+
+term2_tdm_freq <- sort(term2_tdm_freq, decreasing = TRUE)
+
+term2_tdm_freq[1:10]
+
+barplot(term2_tdm_freq[1:10], col = "tan", las = 2)
